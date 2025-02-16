507f3c638a06bb2e4d30a9196c80f454c682af70
Pinar Olguc <pinar.olguc@automattic.com>
2025-02-16 18:58:47 +0300

    Add the xcode project

---
+++ TestiOSApp/TestiOSApp/TestiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestiOSAppApp.swift
+//  TestiOSApp
+//
+//  Created by Pinar Olguc on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestiOSApp/TestiOSAppTests/TestiOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestiOSAppTests.swift
+//  TestiOSAppTests
+//
+//  Created by Pinar Olguc on 16.02.2025.
+//
+
+import Testing
+@testable import TestiOSApp
+
+struct TestiOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
