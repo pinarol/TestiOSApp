b16eba3cfe585f41b5a4a61eb52816a3834ed481
Pinar Olguc <pinar.olguc@automattic.com>
2025-02-17 12:01:41 +0300

    Handle user activity

--- TestiOSApp/Gravatar App Clip/Gravatar_App_ClipApp.swift
+++ TestiOSApp/Gravatar App Clip/Gravatar_App_ClipApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct Gravatar_App_ClipApp: App {
+    @State private var shouldCreateAvatar: Bool = false
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { activity in
+                    guard let incomingURL = activity.webpageURL,
+                          let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else { return }
+                    // direct to the linked content
+                    if components.path?.starts(with: "create-avatar") == true {
+                        shouldCreateAvatar = true
+                    }
+                }
         }
     }
 }
