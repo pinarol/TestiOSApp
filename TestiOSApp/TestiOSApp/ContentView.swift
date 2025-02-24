58cb4f0876f0776efb306e96afe23f753746e01b
Pinar Olguc <pinar.olguc@automattic.com>
2025-02-24 23:08:26 +0300

    Add "Save page"

--- TestiOSApp/TestiOSApp/ContentView.swift
+++ TestiOSApp/TestiOSApp/ContentView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 import CanvasEditor
 
+enum NavigationElement: Hashable {
+    case imageResult(image: UIImage)
+}
+
 struct ContentView: View {
-    @State private var selectedImage: UIImage?
+   // @State private var selectedImage: UIImage?
+    @State private var path: [NavigationElement] = []
+    
     var body: some View {
-        //VStack {
+        NavigationStack(path: $path) {
             CanvasEditorView(inputImage: nil) { image in
-                selectedImage = image
+                //selectedImage = image
+                path.append(.imageResult(image: image))
             } onCancel: {
                 
             }
             .background(Color.black)
             .statusBarHidden(true)
             .ignoresSafeArea(.all)
-        //}
-        //.padding()
+            .navigationDestination(for: NavigationElement.self) { dest in
+                switch dest {
+                case .imageResult(let image):
+                    imageResultView(image: image)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func imageResultView(image: UIImage) -> some View {
+        ImageResultView(image: image)
     }
 }
 
---
+++ TestiOSApp/TestiOSApp/ImageResultView.swift
@@ -0,0 +1,166 @@
+import Foundation
+import SwiftUI
+import GravatarUI
+
+struct ImageResultView: View {
+    let image: UIImage
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(uiImage: image)
+                .resizable()
+                .frame(width: 300, height: 300)
+          //  ScrollView {
+                VStack(alignment: .leading) {
+                    menuButton(
+                        icon: {
+                            Image(uiImage: UIImage(named: "gravatar")!.withRenderingMode(.alwaysTemplate))
+                                .resizable()
+                                .background(Color.white)
+                                .foregroundColor(Color(UIColor.gravatarBlue))
+                        },
+                        title: "Save to Gravatar",
+                        action: {
+                            print("")
+                        }
+                    )
+                    
+                    menuButton(
+                        icon: {
+                            Image(uiImage: UIImage(named: "apple-photos")!)
+                                .resizable()
+                                .background(Color.white)
+                        },
+                        title: "Save to camera roll",
+                        action: {
+                            
+                        }
+                    )
+                }
+        //    }
+            .padding(.top, 24)
+          
+            Spacer()
+            
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Want to access more avatar designs?")
+                        .foregroundColor(Color(uiColor: .label))
+                        .font(Font.system(size: 23, weight: .semibold, design: .monospaced))
+                        .foregroundStyle(.thickMaterial)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+
+                }
+                .padding(.horizontal, 12)
+                .padding(.top, 12)
+                .padding(.bottom, 16)
+
+                VStack(alignment: .leading)  {
+                    Text("Get the full app")
+                        .font(.callout.weight(.semibold))
+                        .foregroundColor(Color(uiColor: .secondaryLabel))
+                    Divider()
+                        .background(Color(uiColor: .secondaryLabel))
+                    HStack(alignment: .center, spacing: 12) {
+                        Image(uiImage: UIImage(named: "full-app-icon")!)
+                            .resizable()
+                            .frame(width: 52, height: 52)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+
+                        VStack(alignment: .leading, spacing: 0) {
+                            HStack {
+                                Image(uiImage: UIImage(named: "appstore")!.withRenderingMode(.alwaysTemplate))
+                                    .resizable()
+                                    .frame(width: 16, height: 16)
+                                Text("App Store")
+                                    .font(.caption)
+                            }
+                            .foregroundColor(Color(uiColor: .secondaryLabel))
+                            .padding(0)
+                            
+                            Text("Gravatar")
+                                .font(.headline.weight(.semibold))
+                                .padding(.bottom, 4)
+                            
+                            Text("Create your global avatar.")
+                                .fixedSize(horizontal: false, vertical: true)
+                                .font(.footnote)
+                                .foregroundColor(Color(uiColor: .secondaryLabel))
+
+
+                        }
+                        Spacer()
+                        Button {
+                        } label: {
+                            Text("GET")
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 16)
+                                .background(Color(UIColor.tintColor))
+                                .font(Font.system(size: 18, weight: .bold, design: .default))
+                                .foregroundColor(Color(.white))
+                                .clipShape(RoundedRectangle(cornerRadius: 16))
+
+                        }
+
+                    }
+                    .padding(.vertical, 4)
+                }
+
+                .padding(.horizontal, 12)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color(UIColor.systemBackground).opacity(1))
+
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding(.horizontal, 12)
+                .shadow(radius: 12)
+
+
+                //
+                
+
+            }
+            
+            .background(Color(UIColor.secondarySystemBackground).opacity(1))
+           
+        }
+        .padding(.top, 24)
+        
+      //
+        .navigationTitle("Save")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    func menuButton(icon: () -> some View, title: String, action: @escaping () -> Void) -> some View {
+        Button {
+            action()
+        } label: {
+            HStack(spacing: 8) {
+                icon()
+                    .frame(width: 24, height: 24)
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                Text(title)
+            }
+            .foregroundStyle(Color(UIColor.label))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 8)
+            .padding(.horizontal, 16)
+            .background(Color(UIColor.secondarySystemBackground))
+            .clipShape(RoundedRectangle(cornerRadius: 6))
+        }
+    }
+}
+
+#Preview {
+    ImageResultView(image: UIImage(named: "avatar-sample")!)
+}
+
+extension UIColor {
+    static let gravatarBlue: UIColor = .rgba(29, 79, 196)
+    
+    static func rgba(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat, alpha: CGFloat = 1.0) -> UIColor {
+        UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: alpha)
+    }
+}
